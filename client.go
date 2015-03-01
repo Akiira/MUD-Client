5d96f8f04227e2f7c1e533c1d09c702b05629a8b
Randall Hudson <Hudsonrj09@gmail.com>
2015-02-28 19:03:33 -0500

    added some code to create a test Gob decocer and encoder for sending message to and from server

--- client.go
+++ client.go
@@ -2,9 +2,48 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
+	"net"
+	"os"
 )
 
+type ClientMessage struct {
+	command int
+	value   string
+}
+
+type ServerMessage struct {
+	value string
+}
+
 func main() {
-	fmt.Println("Hello World!")
+
+	service := "127.0.0.1:1200"
+
+	conn, err := net.Dial("tcp", service)
+	checkError(err)
+
+	encoder := gob.NewEncoder(conn)
+	decoder := gob.NewDecoder(conn)
+
+	message := ClientMessage{1, "test message"}
+	encoder.Encode(message)
+
+	var serversResponse ServerMessage
+	decoder.Decode(&serversResponse)
+	fmt.Println(serversResponse.value)
+
+	os.Exit(0)
+}
+
+func printFormattedServerMessage(message string) {
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
 }
