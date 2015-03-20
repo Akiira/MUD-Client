f9cbc1597122a5d44b77a72916ed1b6c6692c4f9
mixisbad <pattanapoomy@gmail.com>
2015-03-20 19:16:23 -0400

    try add constants

--- ClientMessage.go
+++ ClientMessage.go
@@ -5,22 +5,38 @@ import (
 	"strings"
 )
 
+//command for system
+const CommandLogin = 101
+const CommandLogout = 102
+
+//command for create user
+const CommandRegister = 111
+
+//command in a room
+const CommandAttack = 11
+const CommandItem = 12
+const CommandLeave = 13 // leave occur the same time with enter the room??
+
+//command between room?
+const CommandJoinWorld = 21 // will change the room occur the same time with leave?
+// probably use after authenticate with login server and move to the first world as well
+
+//this is suppose to be an event
 type ClientMessage struct {
 	Command int
 	Value   string
 }
 
-
-func ClientMessageConstructor(cmd int, val string) ClientMessage{
-	return ClientMessage{ Command: cmd, Value: val}
+func ClientMessageConstructor(cmd int, val string) ClientMessage {
+	return ClientMessage{Command: cmd, Value: val}
 }
 
-func (message *ClientMessage) getPassword() string{
+func (message *ClientMessage) getPassword() string {
 	split := strings.Split(message.Value, " ")
 	return split[1]
 }
 
-func (message *ClientMessage) getUsername() string{
+func (message *ClientMessage) getUsername() string {
 	split := strings.Split(message.Value, " ")
 	return split[0]
 }
