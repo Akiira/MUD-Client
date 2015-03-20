1a580f5eee85e80119cfe63fcae80c74055eb851
mixisbad <pattanapoomy@gmail.com>
2015-03-20 15:12:01 -0400

    connect to server

--- client.go
+++ client.go
@@ -2,10 +2,12 @@
 package main
 
 import (
+	"bufio"
 	"encoding/gob"
 	"fmt"
 	"net"
 	"os"
+	"sync"
 )
 
 type ClientMessage struct {
@@ -17,6 +19,8 @@ type ServerMessage struct {
 	Value string
 }
 
+var net_lock sync.Mutex
+
 func main() {
 
 	service := "127.0.0.1:1200"
@@ -24,23 +28,56 @@ func main() {
 	conn, err := net.Dial("tcp", service)
 	checkError(err)
 
+	/*
+		encoder := gob.NewEncoder(conn)
+		decoder := gob.NewDecoder(conn)
+
+		message := ClientMessage{Command: 1, Value: "test message"}
+		fmt.Println("Sending message")
+
+		encoder.Encode(message)
+		fmt.Println("message sent")
+
+		var serversResponse ServerMessage
+		fmt.Println("waiting for response")
+		decoder.Decode(&serversResponse)
+		fmt.Println("message received")
+		fmt.Println(serversResponse.Value)
+
+		conn.Close()
+		os.Exit(0)*/
+
+	go receiveMessage(conn)
+
 	encoder := gob.NewEncoder(conn)
-	decoder := gob.NewDecoder(conn)
+	reader := bufio.NewReader(os.Stdin)
+	for {
 
-	message := ClientMessage{Command: 1, Value: "test message"}
-	fmt.Println("Sending message")
+		fmt.Print("Enter text: ")
+		text, _ := reader.ReadString('\n')
+		message := ClientMessage{Command: 1, Value: text}
+		//net_lock.Lock()
+		encoder.Encode(message)
+		//net_lock.Unlock()
 
-	encoder.Encode(message)
-	fmt.Println("message sent")
+	}
+
+}
+
+func receiveMessage(conn net.Conn) {
 
 	var serversResponse ServerMessage
-	fmt.Println("waiting for response")
-	decoder.Decode(&serversResponse)
-	fmt.Println("message received")
-	fmt.Println(serversResponse.Value)
-
-	conn.Close()
-	os.Exit(0)
+	decoder := gob.NewDecoder(conn)
+	for {
+		//net_lock.Lock()
+		err := decoder.Decode(&serversResponse)
+		//net_lock.Unlock()
+		checkError(err)
+		if err == nil {
+			fmt.Println("message received")
+			fmt.Println(serversResponse.Value)
+		}
+	}
 }
 
 func printFormattedServerMessage(message string) {
