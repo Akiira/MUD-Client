ea27798330c9ffb1741ec8f77a7fa47abd51569f
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-23 03:44:39 -0400

    added new test function to test new features. Also added non blocking read function to always read the servers response.

--- client.go
+++ client.go
@@ -23,8 +23,68 @@ var net_lock sync.Mutex
 
 func main() {
 
-	Test()
+	Test2()
 	os.Exit(0)
+}
+
+func Test2() {
+	service := "127.0.0.1:1200"
+
+	conn, err := net.Dial("tcp", service)
+	checkError(err)
+
+	encoder := gob.NewEncoder(conn)
+	decoder := gob.NewDecoder(conn)
+
+	message := ClientMessage{Command: "initialMessage", Value: "Ragnar"}
+	encoder.Encode(message)
+	go nonBlockingRead(decoder)
+	getInputFromUser(encoder)
+}
+
+func getInputFromUser(encoder *gob.Encoder) {
+	for {
+		var msg ClientMessage
+		var input string
+		read, err := fmt.Scan(&input)
+		checkError(err)
+		_ = read
+
+		if input == "exit" {
+			msg.setToExitMessage()
+			break
+		} else if input == "attack" {
+			var target string
+			read, err = fmt.Scan(&target)
+			msg.setToAttackMessage(target)
+		} else if input == "look" {
+			var target string
+			read, err = fmt.Scan(&target)
+			msg.setToLookMessage(target)
+		} else if input == "get" {
+			var item string
+			read, err = fmt.Scan(&item)
+			msg.setToGetMessage(item)
+		} else { //assume movement
+			msg.setToMovementMessage(input)
+		}
+
+		encoder.Encode(msg)
+
+		if msg.Command == "exit" {
+			break
+		}
+	}
+
+	os.Exit(0)
+}
+
+func nonBlockingRead(decoder *gob.Decoder) {
+	for {
+		var serversResponse ServerMessage
+		decoder.Decode(&serversResponse)
+		printFormatedOutput(serversResponse.Value)
+	}
 }
 
 func Test() {
