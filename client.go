11901d51bdc68c86559cbd2a25511a022305a402
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-23 03:48:14 -0400

    removed most of the testing functions

--- client.go
+++ client.go
@@ -32,6 +32,7 @@ func Test2() {
 
 	conn, err := net.Dial("tcp", service)
 	checkError(err)
+	defer conn.Close()
 
 	encoder := gob.NewEncoder(conn)
 	decoder := gob.NewDecoder(conn)
@@ -87,42 +88,6 @@ func nonBlockingRead(decoder *gob.Decoder) {
 	}
 }
 
-func Test() {
-	service := "127.0.0.1:1200"
-
-	conn, err := net.Dial("tcp", service)
-	checkError(err)
-
-	encoder := gob.NewEncoder(conn)
-	decoder := gob.NewDecoder(conn)
-
-	message := ClientMessage{Command: "initialMessage", Value: "Ragnar"}
-	fmt.Println("Sending message")
-
-	encoder.Encode(message)
-	fmt.Println("message sent")
-
-	var serversResponse ServerMessage
-	fmt.Println("waiting for response")
-	decoder.Decode(&serversResponse)
-	fmt.Println("message received: ")
-	//fmt.Println(serversResponse.Value)
-
-	printFormatedOutput(serversResponse.Value)
-
-	message = ClientMessage{Command: "say", Value: "test say"}
-	encoder.Encode(message)
-	fmt.Println("message 2 sent")
-	for {
-		fmt.Println("waiting for response")
-		decoder.Decode(&serversResponse)
-		fmt.Println("Message received: ", serversResponse)
-		printFormatedOutput(serversResponse.Value)
-	}
-
-	conn.Close()
-}
-
 func printFormatedOutput(output []FormattedString) {
 	for _, element := range output {
 		ct.ChangeColor(element.Color, false, ct.Black, false)
@@ -131,88 +96,6 @@ func printFormatedOutput(output []FormattedString) {
 	ct.ResetColor()
 }
 
-func logInTest() {
-	service := "127.0.0.1:1200"
-
-	conn, err := net.Dial("tcp", service)
-	checkError(err)
-
-	encoder := gob.NewEncoder(conn)
-	decoder := gob.NewDecoder(conn)
-
-	message := ClientMessage{Command: "look", Value: "testChar password"}
-	fmt.Println("Sending message")
-
-	encoder.Encode(message)
-	fmt.Println("message sent")
-
-	var serversResponse ServerMessage
-	fmt.Println("waiting for response")
-	decoder.Decode(&serversResponse)
-	fmt.Println("message received")
-	fmt.Println(serversResponse.Value)
-
-	conn.Close()
-}
-
-func gobTest() {
-	service := "127.0.0.1:1200"
-
-	conn, err := net.Dial("tcp", service)
-	checkError(err)
-
-	/*
-		encoder := gob.NewEncoder(conn)
-		decoder := gob.NewDecoder(conn)
-
-		message := ClientMessage{Command: 1, Value: "test message"}
-		fmt.Println("Sending message")
-
-		encoder.Encode(message)
-		fmt.Println("message sent")
-
-		var serversResponse ServerMessage
-		fmt.Println("waiting for response")
-		decoder.Decode(&serversResponse)
-		fmt.Println("message received")
-		fmt.Println(serversResponse.Value)
-
-		conn.Close()
-		os.Exit(0)*/
-
-	go receiveMessage(conn)
-
-	encoder := gob.NewEncoder(conn)
-	reader := bufio.NewReader(os.Stdin)
-	for {
-
-		fmt.Print("Enter text: ")
-		text, _ := reader.ReadString('\n')
-		message := ClientMessage{Command: "test", Value: text}
-		//net_lock.Lock()
-		encoder.Encode(message)
-		//net_lock.Unlock()
-
-	}
-
-}
-
-func receiveMessage(conn net.Conn) {
-
-	var serversResponse ServerMessage
-	decoder := gob.NewDecoder(conn)
-	for {
-		//net_lock.Lock()
-		err := decoder.Decode(&serversResponse)
-		//net_lock.Unlock()
-		checkError(err)
-		if err == nil {
-			fmt.Println("message received")
-			fmt.Println(serversResponse.Value)
-		}
-	}
-	conn.Close()
-}
 func checkError(err error) {
 	if err != nil {
 		fmt.Println("Fatal error ", err.Error())
